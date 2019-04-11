ef58f18b539e3c591dc2f25675947f94fdadceb6
Kareem Arab <kareemarab@cmail.carleton.ca>
2019-04-11 12:50:28 -0400

    dev->prod

--- config/database.php
+++ config/database.php
@@ -43,8 +43,7 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-        
-        /* This is for deployment
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => RDS_HOSTNAME,
@@ -52,22 +51,6 @@ return [
             'database' => RDS_DB_NAME,
             'username' => RDS_USERNAME,
             'password' => RDS_PASSWORD,
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-        */
-
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
